d27e255ac17c4730d122bc5a5a1f0a02dbaa6e9c
ookil <widomskamg@gmail.com>
2022-11-22 17:45:25 +0100

    global pipe (not working)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
+const PORT = 5000;
+
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {abortOnError: false});
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  await app.listen(PORT, () => console.log(`App running on port ${PORT}`));
 }
 bootstrap();
